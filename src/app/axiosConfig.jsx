28b503cf1d8f6fa99a5f8a3277cb9f06fce00e38
Your Name <you@example.com>
2025-06-17 14:28:55 +0530

    ClusterAssignment

--- src/app/axiosConfig.jsx
+++ src/app/axiosConfig.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 const axiosConfig = axios.create({
-    baseURL: import.meta.env.VITE_APP_BASE_URL,
-    // baseURL: "http://localhost:5000/",
+   // baseURL: import.meta.env.VITE_APP_BASE_URL,
+    baseURL: "http://localhost:5000/",
     headers: {
       'Content-Type': 'application/json'
     }
