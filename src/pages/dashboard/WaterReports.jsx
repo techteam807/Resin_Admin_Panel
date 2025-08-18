31e5b46416ea6ee713397e63d9115c62d7ee46a1
InfoLazeTech <deepakinf30@gmail.com>
2025-08-18 03:03:35 -0700

    waterReports Date bug solve

--- src/pages/dashboard/WaterReports.jsx
+++ src/pages/dashboard/WaterReports.jsx
@@ -28,7 +28,7 @@ function WaterReports() {
 
   useEffect(() => {
     const newFirstDay = `${year}-${String(month).padStart(2, '0')}-01`;
-    const newLastDay = new Date(year, month, 0).getDate();
+    const newLastDay = new Date(Date.UTC(year, month, 0)).getUTCDate();
     const newEndDate = `${year}-${String(month).padStart(2, '0')}-${String(newLastDay).padStart(2, '0')}`;
 
     setStartDate(newFirstDay);
@@ -46,9 +46,9 @@ function WaterReports() {
     dispatch(getWaterReports({ month: formattedMonth, year, startDate, endDate }));
   }, [dispatch, month, year, startDate, endDate]);
 
-  const getDaysInMonth = (month, year) => {
-    return new Date(year, month, 0).getDate();
-  };
+   const getDaysInMonth = (month, year) => {
+   return new Date(Date.UTC(year, month, 0)).getUTCDate();
+ };
 
 
   const daysInMonth = getDaysInMonth(month, year)
@@ -107,7 +107,8 @@ function WaterReports() {
 
     // Loop through each report for this customer
     customer.reports.forEach((report) => {
-      const day = new Date(report.date).getDate();
+    const day = new Date(report.date).getUTCDate();
+
 
       if (!groupedData[userId].scores[day]) {
         groupedData[userId].scores[day] = [];
